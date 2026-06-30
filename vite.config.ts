9a081df9cc325e393c2befdc60bb744089176720
Rendy <Rendyyanisusanto@gmail.com>
2026-06-30 13:38:28 +0700

    chore: set build target to esnext in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  build: {
+    target: 'esnext'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
